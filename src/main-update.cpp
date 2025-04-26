7931ddd570e13f5add331d089c6492be0f6a8dfd
t-kenma <k_tashiro@pocket-lab.jp>
2025-04-26 15:04:00 +0900

    modifi main-update

--- src/main-update.cpp
+++ src/main-update.cpp
@@ -22,16 +22,28 @@
 int main(int argc, char **argv)
 {
     int rc;
+    pid_t pid;
+    int status;
+    
     spdlog::set_level(spdlog::level::debug);
 
     std::shared_ptr<nvr::gpio_out> led_board_green = std::make_shared<nvr::gpio_out>("193", "P9_1");
     std::shared_ptr<nvr::gpio_out> led_board_red = std::make_shared<nvr::gpio_out>("200", "P10_0");
     std::shared_ptr<nvr::gpio_out> led_board_yel = std::make_shared<nvr::gpio_out>("192", "P9_0");
     std::shared_ptr<nvr::logger> logger = std::make_shared<nvr::logger>("/etc/nvr/video-recorder.log");
-
-    rc = nvr::do_systemctl("start", "nvr");
-    SPDLOG_INFO("systemctl start nvr result: {}", rc);
- 
+    
+ 	pid = fork();
+    if (pid < 0) {
+        SPDLOG_ERROR("Failed to fork process: {}", strerror(errno));
+        return -1;
+    } else if (pid == 0) {
+        execl("/usr/bin/nvr", "/usr/bin/nvr", "-r", "now", nullptr);
+        SPDLOG_ERROR("Failed to exec nvr.");
+        exit(-1);
+    }
+    
+    waitpid(pid, &status, 0);
+    
     std::exit(0);
 }
 
