ea6e165b33a2a44cfd6990887b8c6828d070c95f
t-kenma <k_tashiro@pocket-lab.jp>
2025-04-26 16:34:25 +0900

    main-update add signal

--- src/main-update.cpp
+++ src/main-update.cpp
@@ -17,6 +17,54 @@
 #include "sd_manager.hpp"
 #include "util.hpp"
 #include "logging.hpp"
+#include <glib-unix.h>
+
+struct callback_data_t {
+    callback_data_t()
+        : 
+          signal_int_id(0),
+          signal_term_id(0)
+    {}
+    guint signal_int_id;
+    guint signal_term_id;
+
+    std::atomic<bool> interrupted;
+    
+};
+
+int loop = 1;
+
+static void on_interrupted(callback_data_t *data)
+{
+    loop = 0;
+
+}
+
+static gboolean signal_intr_cb(gpointer udata)
+{
+    SPDLOG_INFO("SIGINTR receiverd.");
+
+    callback_data_t* data = static_cast<callback_data_t*>(udata);
+
+    on_interrupted(data);
+
+    /* remove signal handler */
+    data->signal_int_id = 0;
+    return G_SOURCE_REMOVE;
+}
+
+static gboolean signal_term_cb(gpointer udata)
+{
+    SPDLOG_INFO("SIGTERM receiverd.");
+
+    callback_data_t* data = static_cast<callback_data_t*>(udata);
+
+    on_interrupted(data);
+
+    /* remove signal handler */
+    data->signal_term_id = 0;
+    return G_SOURCE_REMOVE;
+}
 
 
 int main(int argc, char **argv)
@@ -27,11 +75,16 @@ int main(int argc, char **argv)
     
     spdlog::set_level(spdlog::level::debug);
     SPDLOG_INFO("main-update");
+    
+    callback_data_t data{};
 
     std::shared_ptr<nvr::gpio_out> led_board_green = std::make_shared<nvr::gpio_out>("193", "P9_1");
     std::shared_ptr<nvr::gpio_out> led_board_red = std::make_shared<nvr::gpio_out>("200", "P10_0");
     std::shared_ptr<nvr::gpio_out> led_board_yel = std::make_shared<nvr::gpio_out>("192", "P9_0");
     std::shared_ptr<nvr::logger> logger = std::make_shared<nvr::logger>("/etc/nvr/video-recorder.log");
+    
+    data.signal_int_id = g_unix_signal_add(SIGINT, G_SOURCE_FUNC(signal_intr_cb), &data);
+    data.signal_term_id = g_unix_signal_add(SIGTERM, G_SOURCE_FUNC(signal_term_cb), &data);
     
  	pid = fork();
     if (pid < 0) {
@@ -43,7 +96,22 @@ int main(int argc, char **argv)
         exit(-1);
     }
     
+    while(loop){
+    
+    }
+    
+    kill(pid, SIGTERM);
     waitpid(pid, &status, 0);
+    SPDLOG_INFO("nvr kill");
+    
+    if (data.signal_int_id)
+    {
+        g_source_remove(data.signal_int_id);
+    }
+    if (data.signal_term_id)
+    {
+        g_source_remove(data.signal_term_id);
+    }
     
     std::exit(0);
 }
