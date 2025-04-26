9459913c7d4253b77ae40a81db9a74774f1b9cd6
t-kenma <k_tashiro@pocket-lab.jp>
2025-04-26 13:29:16 +0900

    add updata process

---
+++ src/main-update.cpp
@@ -0,0 +1,39 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cerrno>
+#include <atomic>
+#include <getopt.h>
+#include <filesystem>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <sys/mount.h>
+#include <sys/signalfd.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <iostream>
+#include "gpio.hpp"
+#include "sd_manager.hpp"
+#include "util.hpp"
+#include "logging.hpp"
+
+
+int main(int argc, char **argv)
+{
+    int rc;
+    spdlog::set_level(spdlog::level::debug);
+
+    std::shared_ptr<nvr::gpio_out> led_board_green = std::make_shared<nvr::gpio_out>("193", "P9_1");
+    std::shared_ptr<nvr::gpio_out> led_board_red = std::make_shared<nvr::gpio_out>("200", "P10_0");
+    std::shared_ptr<nvr::gpio_out> led_board_yel = std::make_shared<nvr::gpio_out>("192", "P9_0");
+    std::shared_ptr<nvr::logger> logger = std::make_shared<nvr::logger>("/etc/nvr/video-recorder.log");
+
+    rc = nvr::do_systemctl("start", "nvr");
+    SPDLOG_INFO("systemctl start nvr result: {}", rc);
+ 
+    std::exit(0);
+}
+
+
+
